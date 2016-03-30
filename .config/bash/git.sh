7ffbaac2358f7fb10485323a60b4f9989209811c
Frederick Wagner <doctaphred@gmail.com>
2016-03-30 17:38:05 -0400

    Fix up git add helpers

--- .config/bash/git.sh
+++ .config/bash/git.sh
@@ -13,15 +13,19 @@ alias regc='git commit --amend'
 alias gch='git checkout'
 alias gcb='git checkout -b'
 alias gchb='git checkout -b'
-alias ga='git add'
-alias gau='git add --update && git status'
-alias gu='git add --update && git status'
 alias gr='git reset'
 alias grs='git reset --soft'
 alias gf='git fetch'
 alias gp='git pull'
 alias gpr='git pull --rebase'
 alias regit='git stash && git pull --rebase && git stash pop'
+
+ga() {
+    git add $@
+    git status
+}
+alias gu='ga --update'
+alias gau='gu'
 
 alias such=git
 alias very=git
