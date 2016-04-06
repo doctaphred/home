24c000c60c330643f5df5ec49e9389ab56064a8c
Frederick Wagner <doctaphred@gmail.com>
2016-04-06 16:31:19 -0400

    Add `git stash list` and `git push` aliases

--- .config/bash/git.sh
+++ .config/bash/git.sh
@@ -3,6 +3,8 @@ alias gsh='git show'
 alias gshn='git show --name-only'
 alias gsn='git show --name-only'
 alias gst='git stash'
+alias gstl='git stash list'
+alias gsl='git stash list'
 alias gsp='git stash pop'
 alias gd='git diff'
 alias gdc='git diff --cached'
@@ -21,6 +23,7 @@ alias gf='git fetch'
 alias gp='git pull'
 alias gpr='git pull --rebase'
 alias regit='git stash && git pull --rebase && git stash pop'
+alias gpu='git push'
 
 ga() {
     git add $@
