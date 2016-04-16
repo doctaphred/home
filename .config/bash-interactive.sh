02101affe6bbd2679f8be595acde56cbc3a30aa8
Frederick Wagner <doctaphred@gmail.com>
2016-04-16 16:38:53 -0400

    Apply personal settings last
    Make sure third-party settings get overwritten

--- .config/bash-interactive.sh
+++ .config/bash-interactive.sh
@@ -1,7 +1,7 @@
 # liquidprompt: https://github.com/nojhan/liquidprompt
 source ~/liquidprompt/liquidprompt
 
-for f in ~/.config/bash-interactive/*; do source $f; done
-
 # fzf: https://github.com/junegunn/fzf
 [ -f ~/.fzf.bash ] && source ~/.fzf.bash
+
+for f in ~/.config/bash-interactive/*; do source $f; done
