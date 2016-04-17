242203d4d5958a96960268959e046925e1e50f1d
Frederick Wagner <doctaphred@gmail.com>
2016-04-17 19:59:52 -0400

    Add comment

--- .config/bash-interactive/python.sh
+++ .config/bash-interactive/python.sh
@@ -4,6 +4,7 @@ export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$HOME/dev
 source /usr/local/bin/virtualenvwrapper.sh
 
+# Disable buffered output to avoid strange problems
 export PYTHONUNBUFFERED=1
 
 # Django localization utilities
