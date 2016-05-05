a2d5dfd7ec0698239759f4d76e7a457ab42b8041
Frederick Wagner <doctaphred@gmail.com>
2016-05-05 16:05:36 -0400

    Fix directory test

--- .config/bash-interactive/vmware.sh
+++ .config/bash-interactive/vmware.sh
@@ -1,3 +1,3 @@
-if [ -f '/Applications/VMware Fusion.app/Contents/Library/' ]; then
+if [ -d '/Applications/VMware Fusion.app/Contents/Library/' ]; then
     export PATH=$PATH:'/Applications/VMware Fusion.app/Contents/Library/'
 fi
