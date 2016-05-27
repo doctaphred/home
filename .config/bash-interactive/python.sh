004615153f26c9aa1fccef8610f125e3fa0799de
Frederick Wagner <doctaphred@gmail.com>
2016-05-26 22:02:03 -0400

    Add system Python aliases

--- .config/bash-interactive/python.sh
+++ .config/bash-interactive/python.sh
@@ -1,6 +1,13 @@
 alias pt='python -m ptpython'
 alias ip='ptipython'
 
+alias syspy2='/usr/local/bin/python2'
+alias syspy3='/usr/local/bin/python3'
+alias syspy=syspy3
+alias syspip2='/usr/local/bin/pip2'
+alias syspip3='/usr/local/bin/pip3'
+alias syspip=syspip3
+
 export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$HOME/dev
 if [ -f /usr/local/bin/virtualenvwrapper.sh ]; then
