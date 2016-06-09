b10e97dc8faa9d24d3f0d236ee3330aaf1b9826a
Frederick Wagner <doctaphred@gmail.com>
2016-06-09 16:24:52 -0400

    Create separate p file
    The previous `sys.argv[0]` trick doesn't play nicely with ptpython
    and/or ipython.

--- .config/python/startup.py
+++ .config/python/startup.py
@@ -1,7 +1,5 @@
-#!/usr/bin/env python
 """
-This file is meant to be executed on REPL startup,
-or directly executed in order to run a single Python command.
+This file is meant to be executed on REPL startup.
 
 See https://docs.python.org/3/using/cmdline.html#envvar-PYTHONSTARTUP
 """
@@ -30,13 +28,3 @@ try:
     import pp
 except ImportError:
     pp = pprint
-
-
-# sys.argv[0] is the empty string if exec'd, as during REPL startup.
-# If this file is instead executed directly, eval its args as Python.
-if sys.argv[0]:
-    args = sys.argv[1:]
-    if not args or args[0] in ['-h', '--help', 'help']:
-        print('Evaluate and print some Python')
-    else:
-        pp(eval(' '.join(args)))
