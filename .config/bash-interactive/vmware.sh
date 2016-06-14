51b65ca7a1c44a60104dc6329d2fbb3fb8f45411
Frederick Wagner <doctaphred@gmail.com>
2016-06-14 15:48:57 -0400

    Add comment to vmware tools

--- .config/bash-interactive/vmware.sh
+++ .config/bash-interactive/vmware.sh
@@ -1,3 +1,6 @@
 if [ -d '/Applications/VMware Fusion.app/Contents/Library/' ]; then
     export PATH=$PATH:'/Applications/VMware Fusion.app/Contents/Library/'
 fi
+
+# list running VMs: `vmrun list`
+# get VM IP: vmrun getGuestIPAddress $PATH_TO_VM
