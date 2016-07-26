d4a3b56c121a4b8851e4c3f1f11900fd22dd9faa
Frederick Wagner <doctaphred@gmail.com>
2016-07-26 14:35:00 -0400

    Don't confirm exit

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -319,7 +319,7 @@ c.TerminalInteractiveShell.autoedit_syntax = True
 # Set to confirm when you try to exit IPython with an EOF (Control-D in Unix,
 # Control-Z/Enter in Windows). By typing 'exit' or 'quit', you can force a
 # direct exit without any confirmation.
-# c.TerminalInteractiveShell.confirm_exit = True
+c.TerminalInteractiveShell.confirm_exit = False
 
 # Set the editor used by IPython (default to $EDITOR/vi/notepad).
 # c.TerminalInteractiveShell.editor = 'subl -w'
