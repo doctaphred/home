229274f555e0ea5690f3ee15137ebdb68225feb4
Frederick Wagner <doctaphred@gmail.com>
2016-11-08 12:56:13 -0500

    Update Django aliases

--- config/bash-interactive/django.sh
+++ config/bash-interactive/django.sh
@@ -5,14 +5,18 @@ alias dj='django-admin'
 
 alias m='./manage.py'
 alias mr='./manage.py runserver'
-alias mrp='./manage.py runserver 0.0.0.0:8000'
+alias mrp='./manage.py runserver 0.0.0.0:8000'  # "runserver (public)"
 alias ms='./manage.py shell'
+alias msh='./manage.py shell'
 alias mm='./manage.py migrate'
 alias mmk='./manage.py makemigrations'
-alias msh='./manage.py showmigrations'
-alias msq='./manage.py squashmigrations'
+alias mmkm='./manage.py makemigrations'
+alias mshm='./manage.py showmigrations'
+alias msqm='./manage.py squashmigrations'
 alias md='./manage.py dumpdata'
+alias mdd='./manage.py dumpdata'
 alias ml='./manage.py loaddata'
+alias mld='./manage.py loaddata'
 alias mt='./manage.py test'
 
 mig () {
