d05d380d9442d88160d698a4c4c80e63251555b9
Frederick Wagner <doctaphred@gmail.com>
2016-11-17 18:05:04 -0500

    Switch aliases to variables

--- config/bash-interactive/python.sh
+++ config/bash-interactive/python.sh
@@ -2,20 +2,22 @@ alias ip='python -m IPython'
 alias ip2='python2 -m IPython'
 alias ip3='python3 -m IPython'
 
+# Declare some variables for use with virtualenv, etc
+
 # Default system Python
-alias syspy='/usr/bin/python'
+syspy='/usr/bin/python'
 
 # Homebrew-installed Python
-alias brewpy2='/usr/local/bin/python2'
-alias brewpy3='/usr/local/bin/python3'
-alias brewpip2='/usr/local/bin/pip2'
-alias brewpip3='/usr/local/bin/pip3'
+brewpy2='/usr/local/bin/python2'
+brewpy3='/usr/local/bin/python3'
+brewpip2='/usr/local/bin/pip2'
+brewpip3='/usr/local/bin/pip3'
 
 # macOS-installer Python
-alias macpy2='/Library/Frameworks/Python.framework/Versions/2.7/bin/python2'
-alias macpip2='/Library/Frameworks/Python.framework/Versions/2.7/bin/pip'
-alias macpy3='/Library/Frameworks/Python.framework/Versions/3.5/bin/python3'
-alias macpip3='/Library/Frameworks/Python.framework/Versions/3.5/bin/pip'
+macpy2='/Library/Frameworks/Python.framework/Versions/2.7/bin/python2'
+macpip2='/Library/Frameworks/Python.framework/Versions/2.7/bin/pip'
+macpy3='/Library/Frameworks/Python.framework/Versions/3.5/bin/python3'
+macpip3='/Library/Frameworks/Python.framework/Versions/3.5/bin/pip'
 
 export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$HOME/dev
